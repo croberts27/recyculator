8a35b5f949e711c284ff26d8d2a9ba16da2dd8a1
Samantha <131906442+Flying-S-Ranch@users.noreply.github.com>
2023-05-31 17:02:34 -0500

    adds event listener for search button

--- assests/scripts/script.js
+++ assests/scripts/script.js
@@ -1,5 +1,6 @@
 // DEPENDENCIES
-
+var searchBtnEl = document.querySelector("#search-button")
+var searchInputEl = document.querySelector("#search-input")
 
 // DATA / STATE
 
@@ -8,22 +9,24 @@
 
 
 // USER INTERACTIONS
-
+searchBtnEl.addEventListener("click", function() {
+  console.log("click")
+})
 
 // INITIALIZATIONS
 
 
-    let map;
+//     let map;
 
-async function initMap() {
-  const { Map } = await google.maps.importLibrary("maps");
-  map = new Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
+// async function initMap() {
+//   const { Map } = await google.maps.importLibrary("maps");
+//   map = new Map(document.getElementById("map"), {
+//     center: { lat: -34.397, lng: 150.644 },
+//     zoom: 8,
+//   });
 
 
-initMap();
-}
+// initMap();
+// }
 
-// AIzaSyAR8nmcULAcf5L45ZkY1-WwYw053-QXGCQ
+// // AIzaSyAR8nmcULAcf5L45ZkY1-WwYw053-QXGCQ
