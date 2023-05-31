90a2edabfd7b2dd80d89c4a32f83a03f3190aeef
Calvin Roberts <you@example.com>
2023-05-31 17:39:56 -0600

    added function to get user location (lon, lat) on window load.

--- assests/scripts/script.js
+++ assests/scripts/script.js
@@ -3,9 +3,41 @@ var searchBtnEl = document.querySelector("#search-button")
 var searchInputEl = document.querySelector("#search-input")
 
 // DATA / STATE
-
+var geoApifyApiKey = "e2e76a6a2e2341f681a4abfd52c7ec27"
 
 // FUNCTIONS
+function getLocation() {
+  // Check if geolocation is supported by the browser
+  if ("geolocation" in navigator) {
+    // Prompt user for permission to access their location
+    navigator.geolocation.getCurrentPosition(
+      // Success callback function
+      (position) => {
+        // Get the user's latitude and longitude coordinates
+        const lat = position.coords.latitude;
+        const lng = position.coords.longitude;
+
+        // Do something with the location data, e.g. display on a map
+        console.log(`Latitude: ${lat}, longitude: ${lng}`);
+      },
+      // Error callback function
+      (error) => {
+        // Handle errors, e.g. user denied location sharing permissions
+        console.error("Error getting user location:", error);
+      }
+    );
+  } else {
+    // Geolocation is not supported by the browser
+    console.error("Geolocation is not supported by this browser.");
+  }
+
+}
+getLocation();
+
+fetch('https://api.geoapify.com/v1/ipinfo?apiKey=' + geoApifyApiKey)
+  .then(response => response.json())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
 
 
 // USER INTERACTIONS
