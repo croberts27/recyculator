720301601792ffe1342895bb69adafcd878797e7
Calvin Roberts <you@example.com>
2023-05-31 18:31:59 -0600

    added geolocator to get lon,lat from user and created a modal for user to enter zip code

--- assests/scripts/script.js
+++ assests/scripts/script.js
@@ -41,9 +41,55 @@ fetch('https://api.geoapify.com/v1/ipinfo?apiKey=' + geoApifyApiKey)
 
 
 // USER INTERACTIONS
-searchBtnEl.addEventListener("click", function() {
+searchBtnEl.addEventListener("DOMContentLoaded", function() {
   console.log("click")
 })
+
+//MODAL CODE
+document.addEventListener('click', () => {
+  // Functions to open and close a modal
+  function openModal($el) {
+    $el.classList.add('is-active');
+  }
+
+  function closeModal($el) {
+    $el.classList.remove('is-active');
+  }
+
+  function closeAllModals() {
+    (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+      closeModal($modal);
+    });
+  }
+
+  // Add a click event on buttons to open a specific modal
+  (document.querySelectorAll('#js-modal-trigger') || []).forEach(($trigger) => {
+    const modal = $trigger.dataset.target;
+    const $target = document.getElementById(modal);
+
+    $trigger.addEventListener('click', () => {
+      openModal($target);
+    });
+  });
+
+  // Add a click event on various child elements to close the parent modal
+  (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+    const $target = $close.closest('.modal');
+
+    $close.addEventListener('click', () => {
+      closeModal($target);
+    });
+  });
+
+  // Add a keyboard event to close all modals
+  document.addEventListener('keydown', (event) => {
+    const e = event || window.event;
+
+    if (e.keyCode === 27) { // Escape key
+      closeAllModals();
+    }
+  });
+});
 
 // INITIALIZATIONS
 
