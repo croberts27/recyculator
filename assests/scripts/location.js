1c8f6f7414fbc938b92159bf605b317c318f1ee6
Calvin Roberts <you@example.com>
2023-06-01 11:43:34 -0600

    commented out getLocation function...not sure if it's needed

--- assests/scripts/location.js
+++ assests/scripts/location.js
@@ -6,38 +6,38 @@ var geoApifyApiKey = "e2e76a6a2e2341f681a4abfd52c7ec27"
 
 //FUNCTIONS
 
-function getLocation() {
-    // Check if geolocation is supported by the browser
-    if ("geolocation" in navigator) {
-      // Prompt user for permission to access their location
-      navigator.geolocation.getCurrentPosition(
-        // Success callback function
-        (position) => {
-          // Get the user's latitude and longitude coordinates
-          const lat = position.coords.latitude;
-          const lng = position.coords.longitude;
+// function getLocation() {
+//     // Check if geolocation is supported by the browser
+//     if ("geolocation" in navigator) {
+//       // Prompt user for permission to access their location
+//       navigator.geolocation.getCurrentPosition(
+//         // Success callback function
+//         (position) => {
+//           // Get the user's latitude and longitude coordinates
+//           const lat = position.coords.latitude;
+//           const lng = position.coords.longitude;
   
-          // Do something with the location data, e.g. display on a map
-          console.log(`Latitude: ${lat}, longitude: ${lng}`);
-        },
-        // Error callback function
-        (error) => {
-          // Handle errors, e.g. user denied location sharing permissions
-          console.error("Error getting user location:", error);
-        }
-      );
-    } else {
-      // Geolocation is not supported by the browser
-      console.error("Geolocation is not supported by this browser.");
-    }
+//           // Do something with the location data, e.g. display on a map
+//           console.log(`Latitude: ${lat}, longitude: ${lng}`);
+//         },
+//         // Error callback function
+//         (error) => {
+//           // Handle errors, e.g. user denied location sharing permissions
+//           console.error("Error getting user location:", error);
+//         }
+//       );
+//     } else {
+//       // Geolocation is not supported by the browser
+//       console.error("Geolocation is not supported by this browser.");
+//     }
   
-  }
-  getLocation();
+//   }
+//   getLocation();
   
   fetch('https://api.geoapify.com/v1/ipinfo?apiKey=' + geoApifyApiKey)
     .then(response => response.json())
     .then(result => console.log(result))
-    .catch(error => console.log('error', error));
+    .catch(error => console.log('error', error))
 
 
 
