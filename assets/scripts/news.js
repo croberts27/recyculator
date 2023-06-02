5be268b9fe704e30888959a1fa5713b4d41b8617
Calvin Roberts <you@example.com>
2023-06-02 15:44:38 -0600

    added fetch request to pull data using media API and used then statements to feed the data back into  HTML elements

--- assets/scripts/news.js
+++ assets/scripts/news.js
@@ -5,15 +5,26 @@ var newsContent3 = document.getElementsByClassName("content-3")
 
 //DATA
 
-var apiKey = 'ba2bdd2816374a669a90bbcd11a3db71';
+var url = "http://api.mediastack.com/v1/news?access_key=ae6dfa625df6421a33447d4a1dcb0e49&keywords=recycling&limit=4";
+
+// var corsURL = "https://cors-anywhere.herokuapp.com/";
+
 
 //FUNCTIONS
 
-var url = 'https://newsapi.org/v2/everthing?' + 'q=Recycling&' + 'from=&' + 'sortBy=popularity&'  + apiKey;
-
-var req = new Request(url);
-
-fetch(req, {mode: 'no-cors'})
-    .then(function(data){
-        console.log(data.json());
+fetch(url)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(news) {
+        console.log(news);
+        document.getElementById("story-1").textContent = news.data[1].title
+        document.getElementById('link-1').textContent = news.data[1].url
+        document.getElementById('link-1').href = news.data[1].url
+        document.getElementById('story-2').textContent = news.data[2].title
+        document.getElementById('link-2').textContent = news.data[2].url
+        document.getElementById('link-2').href = news.data[2].url
+        document.getElementById("story-3").textContent = news.data[3].title
+        document.getElementById('link-3').textContent = news.data[3].url
+        document.getElementById('link-3').href = news.data[3].url
     });
